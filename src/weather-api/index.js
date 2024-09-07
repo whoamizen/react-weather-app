3456b2d87d4707f7a3d8cf2608756ecffc38807e
Mr. Elliot <whoamix91@gmail.com>
2024-09-06 20:03:58 -0500

    reconfig using apikey on .env

--- src/weather-api/index.js
+++ src/weather-api/index.js
@@ -1,5 +1,6 @@
 export const getWeather = async (city) => {
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=es&units=metric&appid=1bc5462df0a917364b4f47ef027014ef`;
+  const appid = import.meta.env.VITE_API_KEY;
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=es&units=metric&appid=${appid}`;
   const response = await fetch(URL);
   const data = await response.json();
   return data;
